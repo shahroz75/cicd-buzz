22dbe7e0459cefee049af7b6f8b159ef73496aaa
shahroz75 <33502352+shahroz75@users.noreply.github.com>
2018-05-20 21:54:46 +0300

    add name

--- app.py
+++ app.py
@@ -11,8 +11,11 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 def generate_buzz():
     page = '<html><body><h1>'
     page += generator.generate_buzz()
-    page += '</h1></body></html>'
+    page += '</h1>
+    page += '<p> Shahroz </p>'
+    page +='</body></html>'
     return page
 
 if __name__ == "__main__":
-app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
+
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 9999)))
