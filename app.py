868027adf0992f7119af0eed6f8469d8a6e9e5d5
shahroz75 <33502352+shahroz75@users.noreply.github.com>
2018-05-21 16:40:23 +0300

    normal

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 def generate_buzz():
     page = '<html><body><h1>'
     page += generator.generate_buzz()
-    page += '</h1><p> Shahroz</p></body></html>'
+    page += '</h1></body></html>'
     return page
 
 if __name__ == "__main__":
